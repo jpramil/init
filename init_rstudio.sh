85c12054afb86274c5d414ddecad37418e2042ab
Julien PRAMIL <100139338+jpramil@users.noreply.github.com>
2024-08-02 12:04:22 +0200

    Update init_rstudio.sh

--- init_rstudio.sh
+++ init_rstudio.sh
@@ -14,26 +14,7 @@ mkdir -p "$RSTUDIO_CONFIG_DIR"
 JSON_CONTENT='{
     "insert_native_pipe_operator": true,
     "posix_terminal_shell": "bash",
-    "editor_theme": "Material",
-    "server_editor_font_enabled": true,
-    "save_workspace": "never",
-    "load_workspace": false,
-    "remove_history_duplicates": true,
-    "auto_detect_indentation": true,
-    "highlight_selected_line": true,
-    "relative_line_numbers": true,
-    "show_invisibles": true,
-    "highlight_r_function_calls": true,
-    "rainbow_parentheses": true,
-    "auto_append_newline": true,
-    "strip_trailing_whitespace": true,
-    "check_unexpected_assignment_in_function_call": true,
-    "style_diagnostics": true,
-    "default_encoding": "UTF-8",
-    "indent_guides": "gray",
-    "syntax_color_console": true,
-    "show_panel_focus_rectangle": true,
-
+    "editor_theme": "Material"
 }'
 # Écrire le contenu JSON dans le fichier de configuration
 echo "$JSON_CONTENT" > "$PREFS_FILE"
