ea69687be4d804bf0cfe6d2d7fb576673d757990
Julien PRAMIL <100139338+jpramil@users.noreply.github.com>
2024-08-02 11:30:20 +0200

    Update init_rstudio.sh

--- init_rstudio.sh
+++ init_rstudio.sh
@@ -25,7 +25,25 @@ fi
 echo "Le script d'initialisation bash a été exécuté."
 
 
-# Ajout du pipi natif --------------------------------------------------------------------
+
+# Utilisation du pipe natif  ------------------------------------------------------------
+RSTUDIO_CONFIG_DIR="$HOME/.config/rstudio/"
+# Fichier de configuration des préférences
+PREFS_FILE="$RSTUDIO_CONFIG_DIR/rstudio_prefs.json"
+# Créer le répertoire de configuration si nécessaire (il ne se passe rien s'il existe déjà)
+mkdir -p "$RSTUDIO_CONFIG_DIR"
+# Contenu JSON pour modifier le raccourci clavier
+JSON_CONTENT='{
+    "insert_native_pipe_operator": true,
+    "posix_terminal_shell": "bash",
+    "editor_theme": "Material"
+}'
+# Écrire le contenu JSON dans le fichier de configuration
+echo "$JSON_CONTENT" > "$PREFS_FILE"
+echo "Native pipe and %aterial theme used"
+
+
+# Ajout de shortcuts  --------------------------------------------------------------------
 
 #!/bin/bash
 
